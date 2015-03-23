824fd576594743482a4b33f1db92f887f8235227
anton2m2m <anton2m2m@hotmail.com>
2015-03-23 11:09:26 +0100

    First
    Very basic ui

---
+++ app/src/main/java/com/example/anton/musicrunner/RunActivity.java
@@ -0,0 +1,15 @@
+package com.example.anton.musicrunner;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Anton on 2015-03-23.
+ */
+public class RunActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_run);
+    }
+}
